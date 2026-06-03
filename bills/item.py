1c8c7aa18c28446918c3b92e004af1143ae58c89
Judit García <judit.garciafer@gmail.com>
2026-06-03 16:17:01 +0200

    Ejercicio 4-6

--- bills/item.py
+++ bills/item.py
@@ -2,51 +2,58 @@ from enum import Enum
 import datetime
 from .entity import *
 
-# Do not change the value of ISD_FACTOR var
+
 ISD_FACTOR = 0.25
 
 
 class TaxType(Enum):
-    # Do not change this enum
     IVA = 1
     ISD = 2
 
 
 class Tax:
-    # Write the parameters in the next line
-    def __init__():
-        # Write here your code
-        pass
+    def __init__(self, tax_id : str, tax_type: TaxType, percentage: float):
+        self.tax_id = tax_id
+        self.tax_type = tax_type
+        self.percentage = percentage
 
 
 class Product:
-     # Write the parameters in the next line
-    def __init__():
-        # Write here your code
-        pass        
+    def __init__(self, product_id: str, name: str, expiration_date: datetime, bar_code: str,quantity: int, price: float, taxes: list):
+        self.product_id = product_id
+        self.name = name
+        self.expiration_date = expiration_date
+        self.bar_code = bar_code
+        self.quantity = quantity
+        self.price = price
+        self.taxes = taxes
 
     def calculate_tax(self, tax: Tax) -> float:
-        # Write here your code
-        pass
+        amount = self.quantity * self.price
+
+        if tax.tax_type == TaxType.ISD:
+            return amount * tax.percentage * ISD_FACTOR
+        
+        return amount * tax.percentage
+    
 
     def calculate_total_taxes(self) -> float:
-        # Write here your code
-        pass
+        total = 0
+        for tax in self.taxes:
+            total += self.calculate_tax(tax)
+        return total
 
     def calculate_total(self) -> float:
-        # Write here your code
-        pass
+        subtotal = self.quantity * self.price
+        return subtotal + self.calculate_total_taxes()
 
     def __eq__(self, another):
-        # Do not change this method
         return hasattr(another, 'product_id') and self.product_id == another.product_id
 
     def __hash__(self):
-        # Do not change this method
         return hash(self.product_id)
 
     def print(self):
-        # Do not change this method
         print(
             f"Product Id:{self.product_id} , name:{self.name}, quantity:{self.quantity}, price:{self.price}")
         for tax in self.taxes:
@@ -55,16 +62,20 @@ class Product:
 
 class Bill:
     def __init__(self, bill_id: str, sale_date: datetime, seller: Seller, buyer: Buyer, products: list[Product]):
-        # Write here your code
-        pass
+        self.bill_id = bill_id
+        self.sale_date = sale_date
+        self.seller = seller
+        self.buyer = buyer
+        self.products = products
        
 
     def calculate_total(self) -> float:
-        # Write here your code
-        pass
+        total = 0
+        for product in self.products:
+            total += product.calculate_total()
+        return total
 
     def print(self):
-        # Do not change this method
         self.buyer.print()
         self.seller.print()
         for product in self.products:
--- bills/stats.py
+++ bills/stats.py
@@ -1,36 +1,67 @@
 # Write your imports here
+from itertools import count, product
 
-
+from .item import Bill, Product
+from .entity import Buyer, Seller
 
 class OrderType:
-    # Do not change this enum
     ASC = 0
     DES = 1
 
 
 class Statistics:
     def __init__(self, bills: list[Bill]):
-        # Do not change this method
         self.bills = bills
 
-    def find_top_sell_product(self) -> (Product, int):
-        # Write here your code
-        pass
+    def find_top_sell_product(self) -> tuple[Product, int]:
+        products_count = {}
+        for bill in self.bills:
+            for product in bill.products:
+                if product.product_id not in products_count:
+                    products_count[product.product_id] = [product, 0]
+                products_count[product.product_id][1] += 1
+        product, count = max(
+        products_count.values(),
+        key=lambda x: x[1]
+        )
+        return (product, count)
+
 
     def find_top_two_sellers(self) -> list:
-        # Write here your code
-        pass
+        totals = {}
+        for bill in self.bills:
+            seller = bill.seller
+            if seller not in totals:
+                totals[seller] = 0
+            totals[seller] += bill.calculate_total()
+        ordered = sorted(totals.items(), key=lambda x: x[1], reverse=True)
+        return [seller for seller, total in ordered[:2]]   
 
-    def find_buyer_lowest_total_purchases(self) -> (Buyer, float):
-        # Write here your code
-        pass
+
+    def find_buyer_lowest_total_purchases(self):
+        totals = {}
+        for bill in self.bills:
+            buyer = bill.buyer
+            if buyer not in totals:
+                totals[buyer] = 0
+            totals[buyer] += bill.calculate_total()
+        buyer = min(totals, key=totals.get)
+        return buyer, totals[buyer]
+
 
     def order_products_by_tax(self, order_type: OrderType) -> tuple:
-        # Write here your code
-        pass
+        products = []
+        for bill in self.bills:
+             for product in bill.products:
+                  products.append(
+                    (product, product.calculate_total_taxes())
+                  )
+        reverse = (order_type == OrderType.DES)
+        products.sort(key=lambda x: x[1], reverse=reverse)
+        return products
+        
 
     def show(self):
-        # Do not change this method
         print("Bills")
         for bill in self.bills:
             bill.print()
